b745d2457f672156176e9725f257e0362c174a47
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-07-10 11:32:43 -0300

    improving category list with base resource list component

--- src/app/shared/components/base-resource-list/base-resource-list.component.ts
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -1,8 +1,9 @@
-import { OnInit } from '@angular/core';
+import { Directive, OnInit } from '@angular/core';
 
 import { BaseResourceModel } from '../../models/base-resource.model';
 import { BaseResourceService } from '../../services/base-resource.service';
 
+@Directive()
 export abstract class BaseResourceListComponent<T extends BaseResourceModel> implements OnInit {
 
   public resources: T[] = [];
@@ -20,7 +21,7 @@ export abstract class BaseResourceListComponent<T extends BaseResourceModel> imp
     const mustDelete = confirm('Deseja realmente excluir este item?');
 
     if (mustDelete) {
-      this.resourceService.delete(resource.id).subscribe(
+      this.resourceService.delete(Number(resource.id)).subscribe(
         () => this.resources = this.resources.filter(element => element != resource)),
         () => alert("Erro ao tentar excluir")
     }
