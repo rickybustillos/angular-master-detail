fa940a528f0605d3e26a5215ccff47c257e974cc
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-07-10 19:56:40 -0300

    adding reports base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,11 @@ const routes: Routes = [
   {
     path: 'categories',
     loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule)
-  }
+  },
+  {
+    path: 'reports',
+    loadChildren: () => import('./pages/reports/reports.module').then(m => m.ReportsModule)
+  },
 ];
 
 @NgModule({
--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { SharedModule } from '../../shared/shared.module';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
