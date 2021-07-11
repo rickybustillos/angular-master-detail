c8edd0180738fe44bf45f640bc4140276c9d7b49
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-07-11 13:10:26 -0300

    improving reports

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -2,11 +2,13 @@ import { Injectable, Injector } from '@angular/core';
 
 
 import { Observable } from 'rxjs';
-import { catchError, mergeMap } from 'rxjs/operators';
+import { catchError, map, mergeMap } from 'rxjs/operators';
 
 import { BaseResourceService } from '../../../shared/services/base-resource.service';
 import { CategoryService } from '../../categories/shared/category.service';
 import { Entry } from './entry.model';
+
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +17,12 @@ export class EntryService extends BaseResourceService<Entry> {
 
   constructor(protected injector: Injector, private categoryService: CategoryService) {
     super("api/entries", injector, Entry.fromJson);
+  }
+
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+    return this.getAll().pipe(
+      map((entries: any) => this.filterByMonthAndYear(entries, month, year))
+    )
   }
 
   create(entry: Entry): Observable<Entry> {
@@ -33,5 +41,16 @@ export class EntryService extends BaseResourceService<Entry> {
       }),
       catchError(this.handleError)
     );
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, 'DD/MM/YYYY');
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if(monthMatches && yearMatches) return entry;
+      else return false;
+    })
   }
 }
